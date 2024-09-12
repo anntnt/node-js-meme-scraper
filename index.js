686bfb494885a3e0cdb8aea4299feafafa21380a
An Tran <anpjure@gmail.com>
2024-09-12 17:43:33 +0200

    Connect website und get image sources using packages axios and cheerio, then download images using modules https and fs

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
 import fs from 'node:fs';
-import http from 'node:https';
+import https from 'node:https';
+import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-const MEMES_URL = 'https://memegen-link-examples-upleveled.netlify.app';
-let html_data;
-const file_list = [];
+// URL of the page we want to scrape
+const url = 'https://memegen-link-examples-upleveled.netlify.app';
+const filenames = [];
 
-http
-  .get(MEMES_URL, function (res) {
-    console.log(res.statusCode);
-    res.setEncoding('utf8');
-    res.on('data', function (data) {
-      console.log(data);
-      //html_data = data;
-    });
-  })
-  .on('error', function (err) {
-    console.log(err);
-  });
-/*const $ = cheerio.load(html_data);
-console.log(pretty($.html()));*/
+// Invoke the function scrapeData() to get list of the first 10 images from the website
+const imageSrcList = await scrapeData();
+imageSrcList.forEach((el, index) => {
+  let filePath;
+  //create file path
+  if (index < 9) filePath = `memes\\0${index + 1}.jpg`;
+  else filePath = `memes\\${index + 1}.jpg`;
+  console.log(el.src + ' : ' + filePath);
+  //download image and save to the file path
+  downloadImage(el.src, filePath);
+});
+//console.log(imageSrcList);
+
+// Async function which scrapes the data
+async function scrapeData() {
+  try {
+    // Fetch HTML of the page we want to scrape
+    const { data } = await axios.get(url);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    // Select all the list items in plainlist class
+    const listItems = $('#images div a');
+    // Stores data for the first 10 image sources
+    const imageLinks = [];
+    // Use for loop through the a we selected
+    for (let i = 0; i < 10; i++) {
+      // Object holding data for each country/jurisdiction
+      let src;
+      const imageLink = { src: '' };
+      src = $(listItems[i]).children('img').attr('src');
+      //remove the substring '?width=300' from image source
+      imageLink.src = src.slice(0, src.indexOf('?'));
+      // Populate imageLinks array with image source
+      imageLinks.push(imageLink);
+    }
+    return imageLinks;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 function downloadImage(url, filepath) {
   return new Promise((resolve, reject) => {
-    http.get(url, (res) => {
+    https.get(url, (res) => {
       if (res.statusCode === 200) {
         res
           .pipe(fs.createWriteStream(filepath))
@@ -39,7 +66,3 @@ function downloadImage(url, filepath) {
     });
   });
 }
-/*downloadImage(
-  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg',
-  'memes/01.jpg',
-);*/
