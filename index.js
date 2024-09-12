b5636939fc543cb0f34605a5c50c8f804890a4a5
An Tran <anpjure@gmail.com>
2024-09-12 20:57:34 +0200

    Change function downloadImage by using module https instead package axios

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import * as client from 'node:https';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
@@ -53,7 +54,7 @@ async function scrapeData(url) {
   }
 }
 
-async function downloadImage(url, filepath) {
+/*async function downloadImage(url, filepath) {
   const response = await axios({
     url,
     method: 'GET',
@@ -64,5 +65,23 @@ async function downloadImage(url, filepath) {
       .pipe(fs.createWriteStream(filepath))
       .on('error', reject)
       .once('close', () => resolve(filepath));
+  });
+}*/
+function downloadImage(url, filepath) {
+  return new Promise((resolve, reject) => {
+    client.get(url, (res) => {
+      if (res.statusCode === 200) {
+        res
+          .pipe(fs.createWriteStream(filepath))
+          .on('error', reject)
+          .once('close', () => resolve(filepath));
+      } else {
+        // Consume response data to free up memory
+        res.resume();
+        reject(
+          new Error(`Request Failed With a Status Code: ${res.statusCode}`),
+        );
+      }
+    });
   });
 }
