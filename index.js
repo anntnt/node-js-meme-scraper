27874e41bd668ed432573b80c38e91d7bd7e2798
An Tran <anpjure@gmail.com>
2024-09-13 11:12:27 +0200

    Fix buges

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ if (!fs.existsSync(DIR)) {
   fs.mkdirSync(DIR);
 }
 
+await scrapeData(URL);
 // Invoke the function scrapeData() to get list of the first 10 images from the website
 /* const imageSrcList = await scrapeData(URL);
 // imageSrcList.forEach((el, index) => {
@@ -31,45 +32,45 @@ for (const [index, el] of imageSrcList.entries()) {
 // console.log(imageSrcList);
 
 // Async function which scrapes the data
-// async function scrapeData(url) {
-try {
-  // Fetch HTML of the page we want to scrape
-  const { data } = await axios.get(URL);
-  // Load HTML we fetched in the previous line
-  const $ = cheerio.load(data);
-  // Select all the list items in plainlist class
-  const listItems = $('#images div a');
-  // Stores data for the first 10 image sources
-  const imageLinks = [];
-  // Use for loop through the a we selected
-  for (let i = 0; i < 10; i++) {
-    // Object holding data for each country/jurisdiction
-    let src = '';
-    const imageLink = { src: '' };
-    src = $(listItems[i]).children('img').attr('src');
-    // remove the substring '?width=300' from image source
-    imageLink.src = src.slice(0, src.indexOf('?'));
-    // Populate imageLinks array with image source
-    imageLinks.push(imageLink);
+async function scrapeData(url) {
+  try {
+    // Fetch HTML of the page we want to scrape
+    const { data } = await axios.get(url);
+    // Load HTML we fetched in the previous line
+    const $ = cheerio.load(data);
+    // Select all the list items in plainlist class
+    const listItems = $('#images div a');
+    // Stores data for the first 10 image sources
+    const imageLinks = [];
+    // Use for loop through the a we selected
+    for (let i = 0; i < 10; i++) {
+      // Object holding data for each country/jurisdiction
+      let src = '';
+      const imageLink = { src: '' };
+      src = $(listItems[i]).children('img').attr('src');
+      // remove the substring '?width=300' from image source
+      imageLink.src = src.slice(0, src.indexOf('?'));
+      // Populate imageLinks array with image source
+      imageLinks.push(imageLink);
+    }
+    // return imageLinks;
+    // const imageLinks = await scrapeData(URL);
+    // imageLinks.forEach((el, index) => {
+    for (const [index, el] of imageLinks.entries()) {
+      let filePath;
+      // create file path
+      if (index < 9) filePath = `${DIR}\\0${index + 1}.jpg`;
+      else filePath = `${DIR}\\${index + 1}.jpg`;
+      // console.log(el.src + ' : ' + filePath);
+      // download image and save to the file path
+      await downloadImage(el.src, filePath)
+        .then(console.log('Downloaded.'))
+        .catch(console.error);
+    }
+  } catch (err) {
+    console.error(err);
   }
-  // return imageLinks;
-  // const imageLinks = await scrapeData(URL);
-  // imageLinks.forEach((el, index) => {
-  for (const [index, el] of imageLinks.entries()) {
-    let filePath;
-    // create file path
-    if (index < 9) filePath = `${DIR}\\0${index + 1}.jpg`;
-    else filePath = `${DIR}\\${index + 1}.jpg`;
-    // console.log(el.src + ' : ' + filePath);
-    // download image and save to the file path
-    await downloadImage(el.src, filePath)
-      .then(console.log('Downloaded.'))
-      .catch(console.error);
-  }
-} catch (err) {
-  console.error(err);
 }
-// }
 
 async function downloadImage(url, filepath) {
   const response = await axios({
